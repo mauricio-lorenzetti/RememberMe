3e9f7335c9a87fabfe26e636fb23db5e51ab5e9d
Mauricio Lorenzetti <mauricio.lorenzetti@icloud.com>
2018-01-15 16:26:23 -0200

    Initial Commit

---
+++ RememberMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RememberMe
+//
+//  Created by Mauricio Lorenzetti Bezerra on 15/01/18.
+//  Copyright © 2018 Mauricio Lorenzetti Bezerra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
