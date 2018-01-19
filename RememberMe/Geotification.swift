2f1802d89205b9e551312405dad880d9174da749
Tarsitani <lucas.tarsitani@gmail.com>
2018-01-19 15:57:23 -0200

    app icon

---
+++ RememberMe/Geotification.swift
@@ -0,0 +1,80 @@
+//
+//  Geotification.swift
+//  RememberMe
+//
+//  Created by Mauricio Lorenzetti Bezerra on 18/01/18.
+//  Copyright © 2018 Mauricio Lorenzetti Bezerra. All rights reserved.
+//
+
+import Foundation
+import MapKit
+import CoreLocation
+
+struct GeotificationKey {
+    static let latitude = "latitude"
+    static let longitude = "longitude"
+    static let radius = "radius"
+    static let identifier = "identifier"
+    static let note = "note"
+    static let eventType = "eventType"
+    static let items = "items"
+}
+
+enum EventType: String {
+    case onEntry = "On Entry"
+    case onExit = "On Exit"
+}
+
+class Geotification: NSObject, NSCoding, MKAnnotation {
+    
+    var coordinate: CLLocationCoordinate2D
+    var radius: CLLocationDistance
+    var identifier: String
+    var note: String
+    var eventType: EventType
+    var items: [Item] = []
+    
+    var title: String? {
+        if note.isEmpty {
+            return "No Note"
+        }
+        return note
+    }
+    
+    var subtitle: String? {
+        let radiusString = String(format: "%.01f", radius)
+        return "Radius: \(radiusString)m"
+    }
+    
+    init(coordinate: CLLocationCoordinate2D, radius: CLLocationDistance, identifier: String, note: String, eventType: EventType, items: [Item]) {
+        self.coordinate = coordinate
+        self.radius = radius
+        self.identifier = identifier
+        self.note = note
+        self.eventType = eventType
+        self.items = items
+    }
+    
+    // MARK: NSCoding
+    required init?(coder decoder: NSCoder) {
+        let latitude = decoder.decodeDouble(forKey: GeotificationKey.latitude)
+        let longitude = decoder.decodeDouble(forKey: GeotificationKey.longitude)
+        coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        radius = decoder.decodeDouble(forKey: GeotificationKey.radius)
+        identifier = decoder.decodeObject(forKey: GeotificationKey.identifier) as! String
+        note = decoder.decodeObject(forKey: GeotificationKey.note) as! String
+        eventType = EventType(rawValue: decoder.decodeObject(forKey: GeotificationKey.eventType) as! String)!
+        items = (decoder.decodeObject(forKey: GeotificationKey.items) as! [Item])
+    }
+    
+    func encode(with coder: NSCoder) {
+        coder.encode(coordinate.latitude, forKey: GeotificationKey.latitude)
+        coder.encode(coordinate.longitude, forKey: GeotificationKey.longitude)
+        coder.encode(radius, forKey: GeotificationKey.radius)
+        coder.encode(identifier, forKey: GeotificationKey.identifier)
+        coder.encode(note, forKey: GeotificationKey.note)
+        coder.encode(eventType.rawValue, forKey: GeotificationKey.eventType)
+        coder.encode(items,forKey:GeotificationKey.items)
+    }
+    
+}
---
+++ RememberMe/ItemDB.swift
@@ -0,0 +1,19 @@
+//
+//  ItemDB.swift
+//  RememberMe
+//
+//  Created by Mauricio Lorenzetti Bezerra on 18/01/18.
+//  Copyright © 2018 Mauricio Lorenzetti Bezerra. All rights reserved.
+//
+
+import Foundation
+
+class ItemsDB: NSObject {
+    
+    //list of objects that will appear in buttonScreen
+    static let allObjects = [ Item(iconTitle: "cat") ]
+    
+    static func getItemByIconTitle(title iconTitle: String) -> Item? {
+        return allObjects.filter{ $0.iconTitle == iconTitle }[0]
+    }
+}
